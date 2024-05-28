9ecadda4d240e9381b7dec897b47fdaf739f1586
letthem <suyeon3662@naver.com>
2024-05-28 21:47:35 +0900

    Feat: naver login

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import NaverProvider from "next-auth/providers/naver";
 
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENTID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENTID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENTID!,
+      clientSecret: process.env.NAVER_SECRET!,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
